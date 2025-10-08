35431d15ea915b3731436eac5cb04ecfc564d7fc
sherrylmosonik <sherrylmosonik@gmail.com>
2025-10-09 01:34:14 +0300

    remove incomplete files

--- questions.js
+++
@@ -1,8 +0,0 @@
-//Write a function that counts vowels in a given string.
-
-//write a function that returns an array of even numbers from a given array
-
-    
-//write a function that takes another function as a parameter(callback)
-
-   
